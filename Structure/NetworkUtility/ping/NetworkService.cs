d792bf38ede56c5eadc5950cc1db8e847bad202c
Mansi Mishra <mansimishra510@gmail.com>
2023-02-11 16:17:52 +0530

    Add files via upload

---
+++ Structure/NetworkUtility/ping/NetworkService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace NetworkUtility.ping
+{
+	public class NetworkService
+	{
+		public string sent()
+			{
+			return "Ping sent";
+			}
+
+		public int ping_Time(int a, int b)
+		{
+			return a + b;
+		} 
+	}
+
+}
+
+
