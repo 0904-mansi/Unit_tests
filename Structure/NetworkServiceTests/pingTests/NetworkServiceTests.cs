384762a101d1da988afaf499ac8c4850d6c7ffb6
Mansi Mishra <mansimishra510@gmail.com>
2023-02-11 16:19:42 +0530

    Add files via upload

---
+++ Structure/NetworkServiceTests/pingTests/NetworkServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xunit;
+using NetworkUtility.ping;
+using FluentAssertions;
+namespace NetworkUtility.Tests.pingTests
+{
+	public class NetworkServiceTests
+	{
+		[Fact]
+		public void NetworkServiceTests1()
+		{
+			var obj = new NetworkService();
+
+			var res = obj.sent();
+
+			res.Should().NotBeNullOrWhiteSpace();
+			res.Should().NotBeNullOrEmpty();
+			res.Should().Be("Ping sent");
+			res.Should().Contain("Ping", Exactly.Once());
+
+		}
+
+		[Theory]
+		[InlineData(1,2,3)]
+		[InlineData(3,4,7)]
+
+		public void return_int(int a, int b, int expected)
+		{
+			var obj = new NetworkService();
+			var res = obj.ping_Time(a,b);
+
+			res.Should().Be(expected);
+			res.Should().BeGreaterThan(2);
+
+		}
+
+	}
+}
+
