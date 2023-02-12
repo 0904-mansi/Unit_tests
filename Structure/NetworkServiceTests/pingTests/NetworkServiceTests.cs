4289078ef14710787b74dc9cd24b9dfe6feb2bb4
Mansi Mishra <mansimishra510@gmail.com>
2023-02-12 15:03:27 +0530

    Update NetworkServiceTests.cs

--- Structure/NetworkServiceTests/pingTests/NetworkServiceTests.cs
+++ Structure/NetworkServiceTests/pingTests/NetworkServiceTests.cs
@@ -1,7 +1,9 @@
-﻿using System;
+using System;
 using Xunit;
 using NetworkUtility.ping;
 using FluentAssertions;
+using FluentAssertions.Extensions;
+using System.Net.NetworkInformation;	
 namespace NetworkUtility.Tests.pingTests
 {
 	public class NetworkServiceTests
@@ -33,7 +35,49 @@ namespace NetworkUtility.Tests.pingTests
 			res.Should().BeGreaterThan(2);
 
 		}
+[Fact]
 
-	}
+		public void return_date()
+		{
+			var res = pingservice.datetime();
+
+			res.Should().BeBefore(1. January(2030));
+			res.Should().BeAfter(1.January(2020));
+		}
+
+		[Fact]
+		public void return_object()
+		{
+
+			var expected = new PingOptions()
+			{
+				DontFragment = true,
+				Ttl = 1
+			};
+
+			var res = pingservice.PingOption();
+			res.Should().BeOfType<PingOptions>();
+			res.Should().BeEquivalentTo(expected);
+			res.Ttl.Should().Be(1);
+		}
+
+
+        [Fact]
+        public void return_ping_object()
+        {
+
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            var res = pingservice.PingOption_();
+            //res.Should().BeOfType<PingOptions>();
+            res.Should().ContainEquivalentOf(expected);
+	    res.Should().Contain(x => x.DontFragment == true);
+        }
+    }
 }
+
 
