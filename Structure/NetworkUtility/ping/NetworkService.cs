c4bf26b9b19458bb9a6aff55f36aead83252a258
Mansi Mishra <mansimishra510@gmail.com>
2023-02-12 15:04:45 +0530

    Update NetworkService.cs

--- Structure/NetworkUtility/ping/NetworkService.cs
+++ Structure/NetworkUtility/ping/NetworkService.cs
@@ -12,8 +12,44 @@ namespace NetworkUtility.ping
 		{
 			return a + b;
 		} 
+		public DateTime datetime()
+		{
+			return DateTime.Now;
+		}
+
+		public PingOptions PingOption()
+		{
+			return new PingOptions()
+			{
+				DontFragment = true,
+				Ttl=1
+			};
+		}
+
+
+        public IEnumerable<PingOptions> PingOption_()
+        {
+			IEnumerable<PingOptions> pingop = new[]{
+
+			new PingOptions(){
+					DontFragment = true,
+				Ttl = 1
+			},
+			 new PingOptions()
+			{
+				DontFragment = true,
+				Ttl = 1
+			},
+			 new PingOptions()
+			{
+				DontFragment = true,
+				Ttl = 1
+			},
+
+			};
+
+			return pingop;
+		}
+	    }
+
 	}
-
-}
-
-
